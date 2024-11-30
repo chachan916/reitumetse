ab0b933862c299425e8479c5d609df69f34641f2
chachan916 <reitumetsemarthachachane@gmail.com>
2024-11-30 17:11:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Get the root element in the HTML to render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside the root element
 root.render(
   <React.StrictMode>
     <App />
